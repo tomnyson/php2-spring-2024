2e4c6a30fb22c6d60207cc288e0bb9e25f5673ce
tomnyson <tabletkindfire@gmail.com>
2024-01-03 21:11:40 +0700

    add code

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+$CONFIG = array(
+    "database" => array(
+        'HOST' => 'localhost',
+        "DB_NAME" => "DB_NAME",
+        "USERNAME" => "root",
+        "PASSWORD" => ""
+    )
+
+);
+
+echo $CONFIG['database']['HOST'];
